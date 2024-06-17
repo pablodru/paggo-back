49f68f3254e3787df3e7d8617786b663555f7f58
pablodru <pdrocha03@gmail.com>
2024-06-17 15:24:42 -0300

    chore: adding port to main

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   
-  await app.listen(5000);
+  const port = process.env.PORT || 5000;
+  await app.listen(port);
 }
 bootstrap();
